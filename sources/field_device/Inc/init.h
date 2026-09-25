f3dc1376eaede2ee121fc652f933810530f3b95d
12354fdw <88530369+12354fdw@users.noreply.github.com>
2026-09-25 22:19:37 +0700

    actual shit in a kettle (added uart pins)
    WRITING REGISTERS IS PAIN!!!!!!!!!

--- sources/field_device/Inc/init.h
+++ sources/field_device/Inc/init.h
@@ -5,9 +5,6 @@
 #ifndef INIT_H
 #define INIT_H
 
-/**
- * @brief Initialize the USART2 debug UART on PA2/PA3 (115200 8N1, HSI16).
- */
 void init(void);
 
 #endif /* INIT_H */
--- sources/field_device/Src/init.c
+++ sources/field_device/Src/init.c
@@ -12,8 +12,17 @@
 #include "stm32c051xx.h"
 
 #define UART_BAUD 115200u
-#define HSI16_HZ  16000000u
+#define HSI16_HZ 16000000u
 
-void uart_init(void) {
-	// TODO: impl this
+void init(void) {
+	// enable clocks
+	RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
+	RCC->APBENR2 |= RCC_APBENR2_USART1EN;
+
+	// set PA9 & PA10 to AF mode
+	GPIOA->MODER &= ~(GPIO_MODER_MODE9_Msk | GPIO_MODER_MODE10_Msk);
+	GPIOA->MODER |=
+		(2U << GPIO_MODER_MODE9_Pos) | (2U << GPIO_MODER_MODE10_Pos);
+
+	
 }
